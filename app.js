c823467585ac1914fe22a29b7b5330ad7b7ab0b0
Milanesa <diegojara88574@gmail.com>
2023-08-26 19:47:44 -0300

    fix: se agregaron comentarios en el app.js para hacerlo mas comprensible, ademas, se establecio la conexion con la base de datos

--- app.js
+++ app.js
@@ -6,21 +6,42 @@ import userRoutes from './routes/userRoutes';
 import playlistRoutes from './routes/playlistRoutes';
 import songRoutes from './routes/songRoutes';
 import dotenv from 'dotenv';
+import { sequelize } from './database/database.js';
 
 dotenv.config();
 
+// Configuracion de la aplicación.
 const app = express();
 const port = process.env.PORT;
 
+
+
+// Middlewares
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
+
+
+// Establecer la conexión con la base de datos
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Conexión a la base de datos establecida correctamente.');
+    })
+    .catch(err => {
+        console.error('No se pudo conectar a la base de datos:', err);
+    });
+
+
+// Definicion de las rutas
 app.use('users', userRoutes);
 app.use('/playlists', playlistRoutes);
 app.use('/songs', songRoutes);
 
+
+//Listen port
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto ${port}`);
 });
