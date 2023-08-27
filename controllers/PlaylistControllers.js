a994335525e6a9301029006643ba602447bd26b0
Milanesa <diegojara88574@gmail.com>
2023-08-27 18:56:13 -0300

    fix: mejoras en el indentado

--- controllers/PlaylistControllers.js
+++ controllers/PlaylistControllers.js
@@ -2,33 +2,33 @@ import Playlist from '../models/playlist.js';
 
 // Funciones de controllerspara las playlist
 export const createPlaylist = async (req, res) => {
-    try {
-       const { name } = req.body;
-       const newPlaylist = await Playlist.create({ name });
-       res.status(201).json(newPlaylist);
-    } catch (error) {
-       console.error('Error al crear la playlist:', error);
-       res.status(500).json({ error: 'Error al crear la playlist' });
+     try {
+         const { name } = req.body;
+          const newPlaylist = await Playlist.create({ name });
+         res.status(201).json(newPlaylist);
+    }    catch (error) {
+         console.error('Error al crear la playlist:', error);
+         res.status(500).json({ error: 'Error al crear la playlist' });
     }
 };
 
 
 export const getAllPlaylists = async (req, res) => {
- try { 
-    const playlists = await Playlist.findAll();
-    res.json(playlists);
- } catch (error) {
-    res.status(500).json ({ error: 'Error al obtener las listas de reproduccion' });
+     try { 
+          const playlists = await Playlist.findAll();
+          res.json(playlists);
+ }   catch (error) {
+     res.status(500).json ({ error: 'Error al obtener las listas de reproduccion' });
  }
 };
 
 export const getPlaylistById = async (req, res) => {
-try { 
-    const playlist = await Playlist.findByPk(req.params.id);
-    if (playlist) {
-        res.json(playlist);
+     try { 
+         const playlist = await Playlist.findByPk(req.params.id);
+         if (playlist) {
+         res.json(playlist);
     } else {
-        res.status(404).json({ error: 'Playlist no encontrada' });
+         res.status(404).json({ error: 'Playlist no encontrada' });
     }
 } catch (error) {
     res.status(500).json({ error: 'Errpr al obtener la Playlist'});
@@ -36,31 +36,31 @@ try {
 };
 
 export const updatePlaylist = async (req, res) => {
- try {
-    const { name } = req.body;
-    const [updatedRowsCount, updatePlaylists ] = await Playlist.update(
-         { name },
-         { where: { id: req.params.id }, returning: true }
+     try {
+         const { name } = req.body;
+      const [updatedRowsCount, updatePlaylists ] = await Playlist.update(
+             { name },
+             { where: { id: req.params.id }, returning: true }
     );
-    if (updatedRowsCount > 0 ) {
+          if (updatedRowsCount > 0 ) {
         res.json(updatePlaylists[0]);
-    } else {
+    }   else {
         res.status(404).json({ error: 'Playlist no encontrada' });
     }
- } catch (error) {
-    res.status(500).json({ error: 'Error al actualizar la playlst' });
+ }      catch (error) {
+        res.status(500).json({ error: 'Error al actualizar la playlst' });
  }
 };
 
 export const deletePlaylist = async (req, res) =>{
- try {
-    const deletedRowCount = await Playlist.destroy({ where: { id: req.params.id } });
-    if (deletedRowCount > 0) {
+     try {
+         const deletedRowCount = await Playlist.destroy({ where: { id: req.params.id } });
+         if (deletedRowCount > 0) {
         res.status(204).send();
-    } else {
+    }   else {
         res.status(404).json({ error: 'Playlist no encontrada' });
     }
- } catch (error) {
+ }  catch (error) {
     res.status(500).json({ error: 'Error al eliminar la playlist'})
  }
 };
--- controllers/songControllers.js
+++ controllers/songControllers.js
@@ -2,63 +2,63 @@ import Song from '../models/song.js'
 
 // Funciones de controllers para las canciones
 export const createSong = async (req, res) => {
- try { 
-    const { title, artist } =req.body;
-    const newSong = await Song.create({ title, artist });
-    res.status(201).json(newSong);
- } catch (error) {
-    res.status(500).json({ error: 'Error al crear la cancion'});
+    try { 
+        const { title, artist } =req.body;
+        const newSong = await Song.create({ title, artist });
+        res.status(201).json(newSong);
+   } catch (error) {
+       res.status(500).json({ error: 'Error al crear la cancion'});
  }
 };
 
 export const GetAllSongs = async (req, res) => {
- try { 
-    const songs = await Song.findAll();
-    res.json(songs);
- } catch (error) {
-    res.status(500).json({ error: 'Error alobtener las canciones' });
+    try { 
+       const songs = await Song.findAll();
+       res.json(songs);
+   } catch (error) {
+       res.status(500).json({ error: 'Error alobtener las canciones' });
  }
 };
 
 export const GetSongById = async (req, res) => {
- try { 
-    const song = await Song.findByPk(req.params.id);
-    if (song) {
+   try { 
+       const song = await Song.findByPk(req.params.id);
+       if (song) {
         res.json(song);
-    } else {
+    }  else {
         res.status(404).json({ error: 'Cancion no encontrada' });
     }
- } catch (error) {
+ }     catch (error) {
     res.status(500).json({ error: 'Error al obtener la cancion' });
  }
 };
 
 export const  updateSong = async (req, res) =>{
- try {
-    const { title, artist } = req.body;
-    const [updatedRowsCount, updateSongs ] = await Song.update(
-        { title, artist },
-        { where: {id: req.params.id }, returning: true }
+   try {
+       const { title, artist } = req.body;
+        const [updatedRowsCount, updateSongs ] = await Song.update(
+           { title, artist },
+           { where: {id: req.params.id }, returning: true }
     );
-    if (updatedRowsCount > 0) {
+        if (updatedRowsCount > 0) {
         res.json(updateSong[0]);
-    } else {
+    }   else {
         res.status(404).json({ error: 'Cancion no encontrada' });
     }
- } catch (error) {
+ }  catch (error) {
     res.status(500).json({ error: 'Error al actualizar la cancion' });
  }
 };
 
 export const DeleteSong = async (req, res) => {
- try {
-    const deletedRowCount = await Song.destroy({ where: { id: req.params.id } });
-    if (deletedRowCount > 0) {
+    try {
+        const deletedRowCount = await Song.destroy({ where: { id: req.params.id } });
+        if (deletedRowCount > 0) {
         res.status(204).send();
-    } else {
+    }  else {
         res.status(404).json({ error: 'Cancion no encontrada' });
     }
- } catch (error) {
+ }  catch (error) {
     res.status(500).json({ error: 'Error al eliminar la cancion' });
  }
 };
